ed40a8aa3e948a33f60b83ff3c07be30e242d1bd
AhmadWasmi <90332025+AhmadWasmi@users.noreply.github.com>
2023-10-27 21:02:16 +0200

    Update mazerunner.bash

--- Assignment 4 Linux/maze/mazerunner.bash
+++ Assignment 4 Linux/maze/mazerunner.bash
@@ -7,17 +7,19 @@ then
     exit
 fi
 
+# Check if curl is installed
+if ! command -v curl &> /dev/null
+then
+    echo "curl could not be found. Please install curl before running this script."
+    exit
+fi
+
 # check if there are any arguments
 if [ $# -eq 0 ]
 then
     echo "No arguments supplied"
     exit 1
 fi
-
-# helping function for displaying horizontal line
-separator () {
-  echo "----------------------------------------"
-}
 
 # Initiate a game and save the game ID in a file.
 init () {
@@ -39,21 +41,18 @@ init () {
     return
   fi
   
-  separator
-  echo "Game has been initialized. The game has the following id:"
+  echo "Game has been initialized. The game has the following ID:"
   echo "${GAME_ID}"
 }
-
 
 # Show which maps are available to choose from.
 maps () {
   MAPS=$(curl -s http://localhost:1337/map | jq -r '.[]')
-  separator
-  echo "Following maps are available: "
+  echo "Following maps are available:"
   I=1
   for MAP in $MAPS
   do
-    echo "$I $MAP"
+    echo "$I. $MAP"
     I=$((I + 1))
   done
   echo "Use ./mazerunner.bash select # to select a map"
@@ -61,129 +60,75 @@ maps () {
 
 # Select a specific map by number.
 select_ () {
-  # read game id from file
   read -r GAME_ID < gameID.txt
-
-  # call api
   MAPS=$(curl -s http://localhost:1337/map | jq -r '.[]')
-
-  # select map that was pick by index
   I=1
-
   for MAP in $MAPS
   do
     if [[ "$I" -eq "$1" ]]
     then
       break
     fi
+    I=$((I + 1))
   done
-
-  # call api to select map
   curl -s "http://localhost:1337/${GAME_ID}/map/{$MAP}"
-
-  # print information
-  separator
-  echo "Following map has been selected: ${I}. ${MAP}"
+  echo "Following map has been selected:"
+  echo "Use ./mazerunner.bash enter to start the game and play manually"
 }
 
 # Enter the first room.
 enter () {
-  # read game id from file
   read -r GAME_ID < gameID.txt
-  # call api
   ROOM=$(curl -s "http://localhost:1337/${GAME_ID}/maze/")
-
-  # print information
-  echo "You have entered the maze."
-  # save room id to file
   echo "$ROOM" | jq -r .roomid > roomID.txt
+  info
 }
 
 # Show information about the room.
 info () {
-  # read game id and room id
   read -r GAME_ID < gameID.txt
   read -r ROOM_ID < roomID.txt
-
-  # call api
   ROOM=$(curl -s "http://localhost:1337/${GAME_ID}/maze/{$ROOM_ID}")
-
-  # print info
-  separator
   echo "You are in room: ${ROOM_ID}"
-
-  # get description if any
   DESCRIPTION=$(echo "$ROOM" | jq -r '.description')
-
-  # create default description
   if [[ -z "$DESCRIPTION" ]]
   then
     DESCRIPTION="Room ${ROOM_ID}"
   fi
-
-  # print description
   echo "Description: $DESCRIPTION"
-
-  # we found the exit, finish the game
-  if [[ "$DESCRIPTION" == "You found the exit" ]]
-  then
-    echo "done" > done.txt
-    return 0
-  fi
-
-  # if we didn't finish keep file empty
-  echo "" > done.txt
-
-  # parse directions
   DIRECTIONS=$(echo "$ROOM" | jq -r '.directions')
-  # store them in file
   echo "$DIRECTIONS" > directions.txt
-
-  # parse where we can go
   EAST=$(echo "$DIRECTIONS" | jq -r '.east')
   SOUTH=$(echo "$DIRECTIONS" | jq -r '.south')
   WEST=$(echo "$DIRECTIONS" | jq -r '.west')
   NORTH=$(echo "$DIRECTIONS" | jq -r '.north')
-
-  # create variable
   CAN_GO=""
-
-  # create text from possible directions
   if [[ "$EAST" != "-" ]]
   then
     CAN_GO="${CAN_GO}east, "
   fi
-
   if [[ "$SOUTH" != "-" ]]
   then
     CAN_GO="${CAN_GO}south, "
   fi
-
   if [[ "$WEST" != "-" ]]
   then
     CAN_GO="${CAN_GO}west, "
   fi
-
   if [[ "$NORTH" != "-" ]]
   then
     CAN_GO="${CAN_GO}north, "
   fi
-
-  # print info
-  echo "You can go: ${CAN_GO}"
+  echo "You can go to: ${CAN_GO}"
 }
 
 # Go to a new room, if the direction is supported.
 go () {
-  # $1 = east, west, north, south
-
-  # read game id and room id
   read -r GAME_ID < gameID.txt
   read -r ROOM_ID < roomID.txt
-
-  # call api and save new room id to file
   ROOM=$(curl -s "http://localhost:1337/${GAME_ID}/maze/${ROOM_ID}/${1}")
   echo "$ROOM" | jq -r .roomid > roomID.txt
+  info
 }
 
 # Automatically solve a map
